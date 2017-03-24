19ca39b16adb5256c680ae621ebe1f7b5bf1946a
S <s>
2017-03-24 14:36:42 -0700

    {"types":"C","runs":56}

--- Breakout.java
+++ Breakout.java
@@ -81,7 +81,6 @@ public class Breakout extends GraphicsProgram {
 	
 	private void buildBrickColumns(int y){
 		int x = (WIDTH - (BRICK_WIDTH)*NBRICKS_PER_ROW)/2 - 19 ;
-		print(WIDTH);
 		for (int i=0; i<NBRICKS_PER_ROW; i++){
 			GRect brick = new GRect(x,y,BRICK_WIDTH,BRICK_HEIGHT);
 			brick.setFilled(true);
