03505af5ad36215e4ca7a1d1cca9769395396a15
S <s>
2017-03-24 14:44:45 -0700

    {"types":"C","runs":63}

--- Breakout.java
+++ Breakout.java
@@ -103,11 +103,11 @@ public class Breakout extends GraphicsProgram {
 		} else if (y==94 || y==106){
 			return Color.ORANGE;
 		} else if(y==118 || y==130){
-		return Color.GREEN;
+		return Color.YELLOW;
 		} else if (y==142 || y==154){
-			return Color.CYAN;
+			return Color.GREEN;
 		} else {
-			return Color.LIGHT_GRAY;
+			return Color.CYAN;
 		}
 	}
 }
