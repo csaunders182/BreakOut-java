5317bdd42c12f1da94efc3485f17077a5e4813be
S <s>
2017-03-26 12:10:25 -0700

    {"types":"C","runs":32}

--- Breakout.java
+++ Breakout.java
@@ -171,11 +171,11 @@ public class Breakout extends GraphicsProgram {
 	}
 	
 	private void collisionLogic(double x, double y){
-		if(getCollidingObject(x,y) == paddle){
-			vy = -vy;
-		} else {
+		if(getCollidingObject(x,y) != paddle){
 			vy = -vy;
 			remove(getCollidingObject(x,y));
+		} else {
+			vy = -vy;	
 		}
 	}
 	
