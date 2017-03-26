4d7e9ffe5c6d50de7e3ea59f17bf9a66c62942b5
S <s>
2017-03-26 16:00:20 -0700

    {"types":"C","runs":88}

--- Breakout.java
+++ Breakout.java
@@ -283,9 +283,4 @@ public class Breakout extends GraphicsProgram {
 	
 	GLabel victoryLabel = new GLabel("Victory", WIDTH/2, HEIGHT/2);
 	
-//	public void mousePressed(MouseEvent e){
-//		if (getElementAt(e.getX(),e.getY()) != null){		
-//			collisionLogic(e.getX(),e.getY());
-//		}
-//	}
 }
